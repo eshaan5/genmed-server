c9a1d5cbec0bea482d0c3e2cde9bcd1bd808981b
eshaan <eshaanbagga@gmail.com>
2023-08-22 12:25:34 +0530

    refactoring index

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import { scrapeMed } from "./scrapers.js";
 import payment from "./payment.js";
 import bodyParser from "body-parser";
 import medicine from "./medicine.js";
@@ -14,19 +13,19 @@ dotenv.config();
 //app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const corsOptions ={
+/*const corsOptions ={
   origin:'http://localhost:3000', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-app.use(function (req, res, next) {
+}*/
+app.use(cors());
+/*app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // disabled for security on local
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
-});
+});*/
 
-app.use(express.json({ extended: false }));
+//app.use(express.json({ extended: false }));
 
 app.use("/payment", payment);
 
