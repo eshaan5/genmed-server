07f3f3f54d2e2e38cfe3bb1f38111c7abf1833e8
eshaan <eshaanbagga@gmail.com>
2023-08-22 12:28:21 +0530

    added cors options

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ dotenv.config();
 //app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-/*const corsOptions ={
+const corsOptions ={
   origin:'http://localhost:3000', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
-}*/
+}
 app.use(cors());
 /*app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // disabled for security on local
