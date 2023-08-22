7f99224f7236689dfd60726e17b3bd1146a2e522
eshaan <eshaanbagga@gmail.com>
2023-08-22 12:47:08 +0530

    updated middleware

--- index.js
+++ index.js
@@ -13,17 +13,14 @@ dotenv.config();
 //app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-app.use(cors());
-/*app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // disabled for security on local
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+//app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
-});*/
+});
 
 //app.use(express.json({ extended: false }));
 
