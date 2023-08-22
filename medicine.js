9dee63f8128b4ff3520a23627986b6f4664f456a
eshaan <eshaanbagga@gmail.com>
2023-08-22 23:08:04 +0530

    returning err

--- medicine.js
+++ medicine.js
@@ -6,8 +6,13 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   console.log(req.body);
   // scrape
+  try {
   const med = await scrapeMed(req.body.name);
   res.json(med);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 export default router;
