0dd5f7903cdbde39d4033cb052d194f558771e65
eshaan <eshaanbagga@gmail.com>
2023-08-22 19:12:27 +0530

    added origin in cors

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ import payment from "./payment.js";
 import bodyParser from "body-parser";
 import medicine from "./medicine.js";
 
-const app = express();  // this we do everytime
-const port = process.env.PORT || 4000;  // this we do everytime
-dotenv.config();  // this we do everytime
+const app = express(); // this we do everytime
+const port = process.env.PORT || 4000; // this we do everytime
+dotenv.config(); // this we do everytime
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));  // images
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));  // to accept the data
-app.use(cors());  // to allow cross-origin resource sharing
+app.use(bodyParser.json({ limit: "30mb", extended: true })); // images
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); // to accept the data
+app.use(
+  cors({
+    origin: ["http://localhost:3000/", "http://example.net"],
+  })
+); // to allow cross-origin resource sharing
 
 app.use("/payment", payment);
 app.use("/medicine", medicine);
